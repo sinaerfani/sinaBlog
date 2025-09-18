f455e847e329491bcdac01d69d0db2ec4d87ec5a
sinaerfani <sinaerfani66@gmail.com>
2025-09-18 08:04:37 +0330

    first run

--- src/main/java/com/example/sinablog/SinaBlogApplication.java
+++ src/main/java/com/example/sinablog/SinaBlogApplication.java
@@ -8,6 +8,7 @@ public class SinaBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SinaBlogApplication.class, args);
+		System.out.println("Start app");
 	}
 
 }
