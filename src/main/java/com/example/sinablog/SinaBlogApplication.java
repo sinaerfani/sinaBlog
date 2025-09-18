3403160c07a913ae87ea3411f25d6bf8a8c914cc
sinaerfani <sinaerfani66@gmail.com>
2025-09-18 07:44:11 +0330

    init project

---
+++ src/main/java/com/example/sinablog/SinaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.sinablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinaBlogApplication.class, args);
+	}
+
+}
